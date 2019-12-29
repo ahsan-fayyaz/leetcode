ccfa8f5891ba55d78bd914a11fb431c6c78fabb4
Ahsan Fayyaz <ahsan@iknox.com>
2019-12-28 20:39:19 -0500

    fixed 1137

--- Algorithms/Easy/1137_N-thTribonnaci.py
+++ Algorithms/Easy/1137_N-thTribonnaci.py
@@ -3,28 +3,31 @@
 # T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 
 # Given n, return the value of Tn.
-def tribonacci(self, n):
+    def tribonacci(self, n):
         """
         :type n: int
         :rtype: int
         """
-        if((n == 0) or (n==1) or (n==2)):
-            return n
+        if(n == 0):
+            return 0
+
+        elif((n==1) or (n==2)):
+            return 1
 
         else:
             t0 = 0
             t1 = 1
             t2 = 1
 
-            for i in range(1,n):
+            for i in range(2,n):
                 next = t0 + t1 + t2
                 t0 = t1
                 t1 = t2
                 t2 = next
 
-            return t1
+            return t2
 
 
 
 #Passed
-#Runtime: 28ms
+#Runtime: 20ms
